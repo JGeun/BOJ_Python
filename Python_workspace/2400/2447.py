083ea4fa8f748a3cf078c8a71cb9dd66cd6bf9e2
JGeun <pjk8926@gmail.com>
2021-02-08 15:55:39 +0900

    별찍기 temp

---
+++ Python_workspace/2400/2447.py
@@ -0,0 +1,23 @@
+def print_star(star, x, y, n):
+    if n == 1:
+        star[y][x] = 1
+        return
+    else:
+        n //= 3
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if i != 1 and j != 1:
+                    print_star(star, x+(j*n), y+(n*i), n)
+
+
+N = int(input())
+star = [[0]*N]*N
+print_star(star, 0, 0, N)
+
+for i in range(0, N):
+    for j in range(0, N):
+        if star[i][j] == 1:
+            print("*", end='')
+        else:
+            print('', end=' ')
+    print()
