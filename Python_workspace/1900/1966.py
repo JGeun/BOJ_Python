a4a88df9b00a6c78ed121aee6fc2e289ad9d4c4d
JGeun <pjk8926@gmail.com>
2021-02-16 23:32:54 +0900

    프린터큐 trying

--- Python_workspace/1900/1966.py
+++ Python_workspace/1900/1966.py
@@ -1,2 +1,18 @@
 import sys
-N = int(sys.stdin.readline())
+T = int(sys.stdin.readline())
+for _ in range(T):
+    N, M = map(int, sys.stdin.readline().split())
+    nums = list(map(int, sys.stdin.readline().split()))
+    queue = [num for num in nums]
+    findValue = nums[M]
+    nums.sort()
+    count = 0
+    index = len(nums)-1
+    while nums[index] != findValue:
+        if queue[0] != nums[index]:
+            queue.append(queue[0])
+            queue.remove(queue[0])
+        else:
+            queue.remove(queue[0])
+            count += 1
+
