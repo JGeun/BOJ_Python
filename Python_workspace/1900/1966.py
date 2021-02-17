8b3c132b41b43757bcba559eb9129e11bade3653
JGeun <pjk8926@gmail.com>
2021-02-17 14:51:56 +0900

    프린터 큐

--- Python_workspace/1900/1966.py
+++ Python_workspace/1900/1966.py
@@ -5,14 +5,21 @@ for _ in range(T):
     nums = list(map(int, sys.stdin.readline().split()))
     queue = [num for num in nums]
     findValue = nums[M]
+    queue[M] = 'check'
     nums.sort()
-    count = 0
+    count = 1
     index = len(nums)-1
     while nums[index] != findValue:
         if queue[0] != nums[index]:
-            queue.append(queue[0])
-            queue.remove(queue[0])
+            queue.append(queue.pop(0))
         else:
-            queue.remove(queue[0])
+            queue.pop(0)
             count += 1
+            index -= 1
 
+    for i in range(len(queue)):
+        if queue[i] == findValue:
+            count += 1
+        elif queue[i] == 'check':
+            sys.stdout.write(str(count)+'\n')
+            break
