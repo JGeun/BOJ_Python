ceac7a5980061551af5c08dd0e4b0ed17e718d68
JGeun <pjk8926@gmail.com>
2021-02-18 17:29:42 +0900

    주유소

--- Python_workspace/13300/13305.py
+++ Python_workspace/13300/13305.py
@@ -2,3 +2,13 @@ import sys
 N = int(sys.stdin.readline())
 distance = list(map(int, sys.stdin.readline().split()))
 oil = list(map(int, sys.stdin.readline().split()))
+needOil = 0
+
+min = oil[0]
+index = 0
+for i in range(len(oil)-1):
+    if min > oil[i]:
+        min = oil[i]
+    needOil += min * distance[i]
+
+sys.stdout.write(str(needOil)+'\n')
